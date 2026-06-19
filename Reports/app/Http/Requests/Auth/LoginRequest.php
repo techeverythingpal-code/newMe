b65b577150f2bea9941d4425c65ebda325a6d456
techeverythingpal-code <tech.every.thing.pal@gmail.com>
2026-06-19 19:31:30 +0300

    another change

--- Reports/app/Http/Requests/Auth/LoginRequest.php
+++ Reports/app/Http/Requests/Auth/LoginRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests\Auth;
 
 use Illuminate\Auth\Events\Lockout;
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
@@ -12,55 +11,37 @@ use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            'email' => ['required', 'string', 'email'],
-            'password' => ['required', 'string'],
+            'SuperVisor_Name' => ['required', 'string'],
+            'password'        => ['required', 'string'],
         ];
     }
 
-    /**
-     * Attempt to authenticate the request's credentials.
-     *
-     * @throws ValidationException
-     */
     public function authenticate(): void
-{
-    $this->ensureIsNotRateLimited();
+    {
+        $this->ensureIsNotRateLimited();
 
-    if (! Auth::attempt([
-        'SuperVisor_Name' => $this->SuperVisor_Name,
-        'password'        => $this->password,
-    ], $this->boolean('remember'))) {
-        RateLimiter::hit($this->throttleKey());
+        if (! Auth::attempt([
+            'SuperVisor_Name' => $this->SuperVisor_Name,
+            'password'        => $this->password,
+        ], $this->boolean('remember'))) {
+            RateLimiter::hit($this->throttleKey());
 
-        throw ValidationException::withMessages([
-            'SuperVisor_Name' => trans('auth.failed'),
-        ]);
+            throw ValidationException::withMessages([
+                'SuperVisor_Name' => trans('auth.failed'),
+            ]);
+        }
+
+        RateLimiter::clear($this->throttleKey());
     }
 
-    RateLimiter::clear($this->throttleKey());
-}
-
-    /**
-     * Ensure the login request is not rate limited.
-     *
-     * @throws ValidationException
-     */
     public function ensureIsNotRateLimited(): void
     {
         if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
@@ -72,18 +53,15 @@ class LoginRequest extends FormRequest
         $seconds = RateLimiter::availableIn($this->throttleKey());
 
         throw ValidationException::withMessages([
-            'email' => trans('auth.throttle', [
+            'SuperVisor_Name' => trans('auth.throttle', [
                 'seconds' => $seconds,
                 'minutes' => ceil($seconds / 60),
             ]),
         ]);
     }
 
-    /**
-     * Get the rate limiting throttle key for the request.
-     */
     public function throttleKey(): string
-{
-    return Str::transliterate(Str::lower($this->string('SuperVisor_Name')).'|'.$this->ip());
-}
+    {
+        return Str::transliterate(Str::lower($this->string('SuperVisor_Name')).'|'.$this->ip());
+    }
 }
