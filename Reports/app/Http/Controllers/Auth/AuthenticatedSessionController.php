11daa1a22ae735d447d1f2b5287847b3bc442196
techeverythingpal-code <tech.every.thing.pal@gmail.com>
2026-06-23 08:08:07 +0300

    updates the destroy method

--- Reports/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ Reports/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -34,14 +34,15 @@ class AuthenticatedSessionController extends Controller
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): RedirectResponse
-    {
-        Auth::guard('web')->logout();
+   public function destroy(Request $request): RedirectResponse
+{
+    Auth::guard('admin')->logout();
+    Auth::guard('web')->logout();
 
-        $request->session()->invalidate();
+    $request->session()->invalidate();
 
-        $request->session()->regenerateToken();
+    $request->session()->regenerateToken();
 
-        return redirect('/');
-    }
+    return redirect('/');
+}
 }
